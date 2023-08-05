e64fc0addbc249dec03f7dcbfb83b5de268b1e50
Name-3R1C <ericfeng94@hotmail.com>
2023-08-05 16:02:24 -0700

    add Appointment comp

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,17 +2,15 @@ import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
 
-export default function InterviewerList(props) {
-  const interviews = props.interviewers.map((interviewer) => {
-    console.log('props ', props);
+export default function InterviewerList({ interviewers, value, onChange}) {
+  const interviews = interviewers.map((interviewer) => {
     return (
       <InterviewerListItem 
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        setInterviewer={props.setInterviewer}
-        selected={interviewer.id === props.interviewer}
-        interviewer={interviewer}
+        selected={interviewer.id === value}
+        setInterviewer={() => onChange(interviewer.id)}    
       />
     )
   });
